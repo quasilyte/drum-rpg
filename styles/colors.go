66e5b415ec16bfd9f672db68ad816bc573718199
Iskander Sharipov <quasilyte@gmail.com>
2024-01-14 12:58:16 +0400

    add styles package

---
+++ styles/colors.go
@@ -0,0 +1,15 @@
+package styles
+
+import (
+	"image/color"
+
+	"github.com/quasilyte/ge"
+)
+
+var (
+	NormalTextColor   = ge.RGB(0xFFFFFF)
+	DisabledTextColor = ge.RGB(0x777777)
+	SelectedTextColor = ge.RGB(0xF4418A)
+
+	TransparentColor = color.RGBA{}
+)
